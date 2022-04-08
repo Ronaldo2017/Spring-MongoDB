84ae78678aa7b75649b463702a4e13deead22b4d
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2022-04-08 09:58:32 -0300

    Busca por titulo do post

--- spring-mongodb/src/main/java/com/rc/springmongo/repository/PostRepository.java
+++ spring-mongodb/src/main/java/com/rc/springmongo/repository/PostRepository.java
@@ -1,6 +1,8 @@
 //este pacote, serve para acesso aos dados
 package com.rc.springmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.rc.springmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
